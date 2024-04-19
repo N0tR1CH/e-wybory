b9e9a592d99d26169e6de4bcee4fc99f64c09c14
N0tR1CH <damianrichter58@gmail.com>
2024-04-19 15:08:58 +0200

    Add example of authorizing swagger routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 require 'rswag/ui/engine'
 
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  authenticate :user, ->(u) { u.role.name == 'user' } do
+    mount Rswag::Ui::Engine, at: '/api-docs'
+    mount Rswag::Api::Engine, at: '/api-docs'
+  end
+
   get 'welcome', to: 'welcome#index'
   get 'welcome/testparameter/:id', to: 'welcome#testparameter'
   get 'welcome/testparameter2/:id', to: 'welcome#testparameter2'
